5e79dc559a1f60add3b999c714ae149382f8dfa1
MKVEERENDRA <136776337+MKVEERENDRA@users.noreply.github.com>
2024-07-12 03:02:26 +0530

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
-import { Web3PluginBase } from "web3";
-
-export class TemplatePlugin extends Web3PluginBase {
-  public pluginNamespace = "template";
-
-  public test(param: string): void {
-    console.log(param);
-  }
-}
-
-// Module Augmentation
-declare module "web3" {
-  interface Web3Context {
-    template: TemplatePlugin;
-  }
-}
+export * from './nft_master_plugin';
